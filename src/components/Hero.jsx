7713a2c442de51b58abd4c081d0bb0486a9847c6
ScrobeZ <jorgeluis-sc@outlook.es>
2025-08-04 20:15:28 -0700

    Hero ahora tiene diseño responsivo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,22 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="flex h-screen w-3/5 items-center justify-between bg-black px-4 text-center text-orange-600"
+      className="flex h-150 w-full flex-col items-center justify-center bg-black px-1 text-center text-orange-600 md:h-screen md:px-0 lg:w-4/5 lg:flex-row lg:justify-evenly"
     >
-      <div>
-        <h1 className="mb-4 text-5xl font-bold">Hola, soy Jorge L. Nieto</h1>
-        <p className="mb-6 w-150 text-xl text-gray-300">
+      <div className="min-w-75 lg:pr-2 xl:px-0">
+        <h1 className="mb-4 text-2xl font-bold md:text-5xl">
+          Hola, soy Jorge L. Nieto.
+        </h1>
+        <p className="mb-6 max-w-150 text-gray-300 md:text-xl">
           Ingeniero en Sistemas | Frontend con React & Flutter | Interés en
           Fullstack (Spring Boot / .NET Core).
         </p>
       </div>
-      <div className="min-h-52 min-w-52 rounded-xl bg-gradient-to-r from-orange-500 to-yellow-400 p-[3px]">
+      <div className="max-w-50 min-w-52 rounded-xl bg-gradient-to-r from-orange-500 to-yellow-400 p-[3px] md:max-w-1/3">
         <img
           src="/assets/foto-graduacion-cuadro.jpeg"
           alt="Jorge L. Nieto"
-          className="oobject-cover h-100 w-100 rounded-lg"
+          className="aspect-square h-full w-full rounded-lg"
         />
       </div>
     </section>
